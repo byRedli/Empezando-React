de69a9e876674fe53327cf884eebcb8098d139a9
RedliDev <ilderyoel06@gmail.com>
2025-01-07 23:34:53 -0500

    Segundo Avance del primer proyecto FollowTwitter

---
+++ 00-hola-mundo/src/TwitterFollowCard.jsx
@@ -0,0 +1,19 @@
+export function TwitterFollowCard ({userName, name, isFollowing}){
+    console.log(isFollowing)
+    // const imgSrc= 'https://unavatar.io/onlyfans/${userName}'
+    return(
+        <article className='tw-followCard'>
+            <header className='tw-followCard-header'>
+                <img className='tw-followCard-avatar' src={`https://unavatar.io/onlyfans/${userName}`} alt="Avatar 1" />
+                <div className='tw-followCard-info'>
+                    <strong className='tw-followCard-text'>{name}</strong>
+                    <span className='tw-followCard-infoUserName'>@{userName}</span>
+                </div>
+            </header>
+            <aside>
+                <button className='tw-followCard-button'>Seguir</button>
+            </aside>
+        </article>
+        
+    )
+}
